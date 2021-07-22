320d459b6953908a069ce5702dbe0b49ce2d4cbf
Dimas Ramadani <59839225+DarkSkull777@users.noreply.github.com>
2021-07-22 15:29:38 +0700

    update callmanager.py

--- callmanager.py
+++ callmanager.py
@@ -58,11 +58,11 @@ class GoupCallInstance(object):
             try:
                 chat_id = context.full_chat.id
                 logInfo(
-                    f"Playout ended, skipping to next song, current file which ended : {args}")
+                    f"Playout berakhir, melompat ke lagu berikutnya, file saat ini yang berakhir : {args}")
                 self.currentRepeatCount = self.currentRepeatCount + 1
                 if self.repeatCount > 1 and self.currentRepeatCount < self.repeatCount:
                     logInfo(
-                        f"Resatrting the playout : Loop : {self.repeatCount} ,Current Count : {self.currentRepeatCount}")
+                        f"Memulai ulang permainan : Loop : {self.repeatCount} ,Current Count : {self.currentRepeatCount}")
                     self.pytgcalls.restart_playout()
                     return
                 await self.skipPlayBack()
@@ -72,16 +72,16 @@ class GoupCallInstance(object):
         @self.pytgcalls.on_network_status_changed
         async def on_network_changed(context, is_connected: bool):
             try:
-                logInfo(f"changing status to : {is_connected}")
+                logInfo(f"berubah status menjadi : {is_connected}")
                 chat_id = context.full_chat.id
                 if is_connected is True:
                     self.active = True
-                    self.status = "playing"
+                    self.status = "bermain"
                 else:
                     await self.stopPlayBack(fromHandler=True)
-                logInfo(f"status changed : {chat_id} {is_connected}")
+                logInfo(f"statusnya berubah : {chat_id} {is_connected}")
             except Exception as ex:
-                logException(f"Error in on_network_changed : {ex}", True)
+                logException(f"Kesalahan di on_network_changed : {ex}", True)
 
     async def changeFile(self, fileName, songInfo, requester, oldFileName=None):
         try:
@@ -93,7 +93,7 @@ class GoupCallInstance(object):
                     os.remove(oldFileName)
             except Exception as ex:
                 self.logException(
-                    f"Error while removing the file{oldFileName} : {ex}", True)
+                    f"Kesalahan saat menghapus file{oldFileName} : {ex}", True)
             if MongoDBClient.client is not None:
                 MongoDBClient.add_song_playbacks(
                     songInfo, requester, self.mongo_doc['_id'])
@@ -106,7 +106,7 @@ class GoupCallInstance(object):
         return self.pytgcalls.is_connected
 
     async def getCurrentCall(self):
-        self.logInfo(f"Making call to get current calls in the chat.")
+        self.logInfo(f"Melakukan panggilan untuk mendapatkan panggilan saat ini dalam obrolan.")
         return await self.pytgcalls.get_group_call(self.chat_id)
 
     async def preCheck(self, botClient, useClient):
@@ -125,8 +125,8 @@ class GoupCallInstance(object):
             await asyncio.sleep(0.0)
         except Exception as ex:
             logWarning(
-                f"Can ignore this => export_chat_invite_link : {self.chat_id} {ex}")
-            return f"🕵️‍♀️**Oops, add the bot as an admin in the chat and grant it all permissions.**"
+                f"Bisa mengabaikan ini => export_chat_invite_link : {self.chat_id} {ex}")
+            return f"🕵️‍♀️**Ups, tambahkan bot sebagai admin di obrolan dan berikan semua izin.**"
         try:
             await useClient.join_chat(invitelink)
             return True
@@ -135,11 +135,11 @@ class GoupCallInstance(object):
         except InviteHashExpired as ie:
             logWarning(
                 f"Can ignore this => User is denied : {self.chat_id} {ie}")
-            return f"__Error while adding helper account `[` {config.get('HELPER_ACT')} `]` in chat.__\n\n**__Make sure the helper account is not in removed users list.__**"
+            return f"__Error saat menambahkan akun Asisten `[` {config.get('HELPER_ACT')} `]` in chat.__\n\n**Pastikan akun Asisten tidak ada dalam daftar pengguna yang dihapus.__**"
         except FloodWait as fe:
             logWarning(
                 f"Can ignore this => User is denied : {self.chat_id} {fe}")
-            return f"__Flood Wait Error while adding helper account `[` {config.get('HELPER_ACT')} `]` in chat.__\n\n**__please wait {fe.x} sec or add the helper accoutn manually.__**"
+            return f"__Flood Wait Error saat menambahkan akun Asisten `[` {config.get('HELPER_ACT')} `]` dalam obrolan.__\n\n**__please wait {fe.x} detik atau tambahkan akun Asisten secara manual.__**"
         except Exception as ex:
             logWarning(
                 f"Can ignore this =>  Error : {self.chat_id} {ex}")
@@ -148,7 +148,7 @@ class GoupCallInstance(object):
     async def start_playback(self, fileName, songInfo, requested_by):
         try:
             self.logInfo(
-                f"Starting the playback in chat : current song queue : {self.songs}")
+                f"Memulai pemutaran di obrolan: antrian lagu saat ini : {self.songs}")
             try:
                 await self.pytgcalls.start(self.chat_id)
             except Exception as e:
@@ -161,22 +161,22 @@ class GoupCallInstance(object):
             return True
         except Exception as ex:
             self.logException(f"Error while starting the playback: {ex}", True)
-            return f"**__{ex}__**\n\n__Please add the heleper account `[` {config.get('HELPER_ACT')} `]` in this chat and then send the command again.__"
+            return f"**__{ex}__**\n\nSilakan tambahkan akun Asisten `[` {config.get('HELPER_ACT')} `]` dalam obrolan ini dan kemudian kirim perintah lagi.__"
 
     async def addSongsInQueue(self, fileName, songInfo, requested_by):
         try:
             self.logInfo(
-                f"Adding song to the queue : {songInfo} , {fileName}")
+                f"Menambahkan lagu ke antrian : {songInfo} , {fileName}")
             if queues.size(self.chat_id) >= config.get('PLAYLIST_SIZE'):
-                return True, f"✖️**__Currently at most {config.get('PLAYLIST_SIZE')} songs can be added in queue. Please try again after some songs end.__**"
+                return True, f"✖️**__Saat ini paling banyak {config.get('PLAYLIST_SIZE')} lagu dapat ditambahkan dalam antrian. Silakan coba lagi setelah beberapa lagu berakhir.__**"
             await queues.put(self.chat_id, file=fileName, song=songInfo, requested_by=requested_by)
             self.songs.append(
                 {"file": fileName, "song": songInfo, "requested_by": requested_by, "time": time.time()})
             req_by = f"[{requested_by['title']}](tg://user?id={requested_by['chat_id']})"
-            return True, f"**✅ Added to queue.**\n**Name:** `{(songInfo['title'].strip())[:20]}`\n**Requester:** {req_by}"
+            return True, f"**✅ Ditambahkan ke antrian.**\n**Name:** `{(songInfo['title'].strip())[:20]}`\n**Peminta:** {req_by}"
         except Exception as ex:
             self.logException(f"Error while addSongsInQueue: {ex}", True)
-            return False, "**__Error while adding song in the queue : {ex}.__**"
+            return False, "**__Error saat menambahkan lagu dalam antrian : {ex}.__**"
 
     async def skipPlayBack(self, fromCommand=False):
         try:
@@ -186,22 +186,22 @@ class GoupCallInstance(object):
             if queues.is_empty(self.chat_id) is True or len(self.songs) == 0:
                 if fromCommand is False:
                     await self.stopPlayBack()
-                return False, f"**__😬 There are no songs waiting in queue, If you want to stop send /stop.__**"
+                return False, f"**__😬 Tidak ada lagu yang menunggu dalam antrian, Jika Anda ingin berhenti mengirim /stop.__**"
             else:
                 old_file = self.songs[0].get('file')
                 self.songs.pop(0)
                 new_song = queues.get(self.chat_id)
                 await self.changeFile(
                     new_song["file"], new_song["song"], new_song['requested_by'], old_file)
-                return True, f"**__Succesfully skipped the song.__**\n\n**Now Playing ▶️** `{new_song['song']['title']}`"
+                return True, f"**__Berhasil melewatkan lagu.__**\n\n**Sedang dimainkan ▶️** `{new_song['song']['title']}`"
         except Exception as ex:
-            self.logException(f"Error while skipPlayBack: {ex}", True)
-            return False, f"**__Error while skipping : {ex}__**"
+            self.logException(f"Kesalahan saat melewatkanPlayBack: {ex}", True)
+            return False, f"**__Kesalahan saat melewatkan : {ex}__**"
 
     async def stopPlayBack(self, fromHandler=False, sendMessage=False, force=False):
         try:
             self.logInfo(
-                f"Stopping the playback : fromHandler : {fromHandler} ")
+                f"Menghentikan pemutaran : fromHandler : {fromHandler} ")
             # delete all downloaded songs in queue
             for s in self.songs:
                 try:
@@ -244,16 +244,16 @@ class GoupCallInstance(object):
                                                     source=506)
                         await self.client.send(leave_call)
                     except Exception as ex:
-                        logWarning(f"Failed to force leave :{ex}")
+                        logWarning(f"Gagal memaksa pergi :{ex}")
 
             await asyncio.sleep(0.1)
-            resp_message = "**Playback ended and thank you 🙏🏻 for trying and testing the service.**\n__Do give your feedback/suggestion @sktechhub_chat.__"
+            resp_message = "**Pemutaran berakhir dan terima kasih 🙏🏻 telah mencoba dan menguji layanan.**\n__Berikan tanggapan/saran Anda @xskull7.__"
             if sendMessage is True and self.bot_client is not None:
-                resp_message = "**Playback ended `[If you were in middle of a song and you are getting this message then this has happended due to a deployement. You can play again after some time.]`**\n\n__Thank you for trying and do give your feedback/suggestion @sktechhub_chat.__"
+                resp_message = "**Pemutaran berakhir `[Jika Anda berada di tengah-tengah sebuah lagu dan Anda mendapatkan pesan ini, maka ini terjadi karena penerapan. Anda dapat bermain lagi setelah beberapa waktu.]`**\n\n__Terima kasih telah mencoba dan memberikan umpan balik / saran Anda @xskull7.__"
                 await self.bot_client.send_message(self.chat_id, f"{resp_message}")
             return True, resp_message
         except BotMethodInvalid as bi:
-            self.logWarn(f"Expected error while stopping the playback : {bi}")
+            self.logWarn(f"Kesalahan yang diharapkan saat menghentikan pemutaran : {bi}")
             return False, f"**__Error while stopping : {bi}__**"
         except Exception as ex:
             self.logException(f"Error while stopping the playback: {ex}", True)
@@ -262,29 +262,29 @@ class GoupCallInstance(object):
     async def getCurrentInfo(self, userId):
         try:
             self.logInfo(
-                f"Getting current info : {self.status}")
-            info = f"**Is Active [ {self.chat_id} ] :** `{self.active}` {'**[** `'+self.songs[0].get('song').get('title')+'` **]**' if len(self.songs)>0 else ''}"
+                f"Mendapatkan info terkini : {self.status}")
+            info = f"**Aktif [ {self.chat_id} ] :** `{self.active}` {'**[** `'+self.songs[0].get('song').get('title')+'` **]**' if len(self.songs)>0 else ''}"
             info = info + \
                 f"\n**Loop:** `{self.repeatCount if self.repeatCount>1 else 'OFF'}`"
             isEmpty = queues.is_empty(self.chat_id)
             if isEmpty is True or len(self.songs) == 0:
-                info = info + f"\n\n__No songs in queue.__"
+                info = info + f"\n\n__Tidak ada lagu dalam antrian.__"
             else:
-                info = info + f"\n\n**Upcoming:**\n"
+                info = info + f"\n\n**Mendatang:**\n"
                 sliced_songs = self.songs[1:]
                 for i in range(len(sliced_songs)):
                     info = info + \
                         f"**{i+1}.** `{sliced_songs[i].get('song').get('title')}`\n"
 
             if userId in [i['chat_id'] for i in config.get('GLOBAL_ADMINS')]:
-                info = info + "\n\n**Admin View:**"
+                info = info + "\n\n**Tampilan Admin:**"
                 music_player = MusicPlayer()
                 count = 1
                 for chat_id, gc_instance in music_player.group_calls.items():
                     info = info + \
-                        f"\n{count}. {chat_id} - **Active:** `{gc_instance.active}` **Status:** `{gc_instance.status}`"
+                        f"\n{count}. {chat_id} - **Aktif:** `{gc_instance.active}` **Status:** `{gc_instance.status}`"
                     info = info + \
-                        f" **Queue:** `{len(gc_instance.songs) if gc_instance.songs is not None else None }` , **Loop:** `{self.repeatCount if self.repeatCount>1 else 'OFF'}`"
+                        f" **Antri:** `{len(gc_instance.songs) if gc_instance.songs is not None else None }` , **Loop:** `{self.repeatCount if self.repeatCount>1 else 'OFF'}`"
                     count = count+1
             return True, f"{info}"
         except Exception as ex:
@@ -320,7 +320,7 @@ class MusicPlayer(metaclass=Singleton):
             else:
                 # check if it can be created
                 if chat_id not in config.get('SUDO_CHAT') and len(self.group_calls) >= self.SIMULTANEOUS_CALLS and force is False:
-                    return None, f"**__😖 Sorry but currently the service is being used in `{len(self.group_calls)}` groups/channels and currently due to lack of resource we support at max `{self.SIMULTANEOUS_CALLS}` simultaneous playbacks.__**"
+                    return None, f"**__😖 Maaf tapi saat ini layanan sedang digunakan di `{len(self.group_calls)}` groups/channels dan saat ini karena kurangnya sumber daya yang kami dukung secara maksimal `{self.SIMULTANEOUS_CALLS}` simultaneous playbacks.__**"
                 logInfo(
                     f"Creating new group call instance : {chat_id} {len(self.group_calls)}")
                 gc = GoupCallInstance(chat_id, mongo_doc, client)
@@ -329,7 +329,7 @@ class MusicPlayer(metaclass=Singleton):
 
         except Exception as ex:
             logException(f"Error while getting group call {ex}", True)
-            return None,  "**__🙄 Unexpected Error, Be assured our best minds have been notified and they are working on it.__**"
+            return None,  "**__🙄 Kesalahan Tak Terduga, Yakinlah pikiran terbaik kami telah diberitahu dan mereka sedang mengerjakannya.__**"
 
     async def shutdown(self):
         try:
